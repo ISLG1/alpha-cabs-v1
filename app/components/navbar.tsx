15892f5829738712c3e221cce6f68c289f58b29c
Anand Loorthu Raj A <garudaoneinc@gmail.com>
2025-03-12 17:09:46 +0530

    form production ready

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,32 +1,33 @@
 import React from 'react'
+import Link from 'next/link'
 
 const navbar = () => {
   return (
     <nav className="navbar navbar-bg sticky top-0">
       <div className="container max-w-7xl mx-auto flex justify-between items-center">
         <div className="navbar-brand p-3">
-          <a href="/" className="navbar-item">
+          <Link href="/" className="navbar-item">
             <img src="/web_logo.png" alt="Logo" className='max-h-12'/>
-          </a>
+          </Link>
         </div>
         <div className="navbar-menu">
           <div className="navbar-content p-2">
-            <a href="#about" className="navbar-item font-normal mx-4">
+            <Link href="#about" className="navbar-item font-normal mx-4">
               About
-            </a>
-            <a href="#services" className="navbar-item font-normal mx-4">
+            </Link>
+            <Link href="#services" className="navbar-item font-normal mx-4">
               Services
-            </a>
-            <a href="#pricing" className="navbar-item font-normal mx-4">
+            </Link>
+            <Link href="#pricing" className="navbar-item font-normal mx-4">
               Pricing
-            </a>
+            </Link>
           </div>
         </div>
         <div className="navbar-cta">
             <div className="navbar-button">
-                <a href="/contact" className="button cta-color font-semibold px-5 py-3 text-black rounded-full">
+                <Link href="/contact" className="button cta-color font-semibold px-5 py-3 text-black rounded-full">
                     BOOK NOW                
-                </a>
+                </Link>
             </div>
         </div>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Navbar from './components/navbar'
 import Form from './components/form'
-import Book from './book'
 
 const page = () => {
   return (
